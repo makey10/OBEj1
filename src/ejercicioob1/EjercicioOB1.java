4672846cbe38e66e50d709a190562b3e886c95d3
Lucas Ale <lucasmartinale123@gmail.com>
2022-08-18 20:44:19 -0300

    Primer commit

---
+++ src/ejercicioob1/EjercicioOB1.java
@@ -0,0 +1,38 @@
+/*Para este primer ejercicio tendréis que realizar lo siguiente:
+
+Crea un proyecto de Java desde 0
+
+Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una clase.
+
+Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las sesiones.
+
+Recordatorio: Los tipos de datos más comunes son int, long, double, boolean, String.
+package ejercicioob1;*/
+
+/**
+ *
+ * @author LUCAS
+ */
+public class EjercicioOB1 {
+
+  
+    public static void main(String[] args) {
+        
+        String nombre = "Lucas";
+        int edad = 25;
+        long dni = 4521754;
+        double altura = 1.85;   
+        boolean aprendiz = true;
+        
+        System.out.println("Nombre: " + nombre);
+        System.out.println("edad: " + edad);
+        System.out.println("DNI: " + dni);
+        System.out.println("Altura: " + altura);
+        System.out.println("Aprendiz ?: " + aprendiz);
+        
+        
+        
+        
+    }
+    
+}
